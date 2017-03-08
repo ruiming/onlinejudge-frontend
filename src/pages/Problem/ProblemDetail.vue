607c5b9c2880196b2c5e005f8f2602cd90a4de3a
ruiming <ruiming.zhuang@gmail.com>
2017-03-08 20:48:42 +0800

    完善问题详情页面

--- src/pages/Problem/ProblemDetail.vue
+++ src/pages/Problem/ProblemDetail.vue
@@ -3,18 +3,45 @@
   <div class="description">
     <p>{{problem.description}}</p>
   </div>
+  <div class="supplement">
+    <div v-for="(item, index) in supplement" :key="index">
+      <h3>{{item.name}}</h3>
+      <p>{{item.value}}</p>
+    </div>
+  </div>
 </div>
 </template>
 
 <script>
 export default {
   name: 'problem-detail',
-  props: ['problem']
+  props: ['problem'],
+  computed: {
+    supplement () {
+      return [{
+        name: 'Input',
+        value: this.problem.input
+      }, {
+        name: 'Output',
+        value: this.problem.output
+      }, {
+        name: 'SampleInput',
+        value: this.problem.sampleInput
+      }, {
+        name: 'SampleOutput',
+        value: this.problem.sampleOutput
+      }]
+    }
+  }
 }
 </script>
 
 <style scoped>
-.description {
+.description,
+.supplement {
   text-align: left;
+}
+.supplement {
+  border-bottom: 1px solid #eee;
 }
 </style>
