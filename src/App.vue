5aa10295a35ae3f4a16453c362ff8f6be1806c43
smilehuhuhuhu <2388071893@qq.com>
2017-05-12 14:34:14 +0800

    主页及导航栏的css

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,24 @@
     <el-menu :default-active="$route.path" :router="true" mode="horizontal">
       <div class="container">
         <el-menu-item index="/">主页</el-menu-item>
-        <el-menu-item index="/problemset">题库</el-menu-item>
+        <el-submenu  >
+          <template slot="title">题库</template>
+          <el-menu-item index="/problemset">C++</el-menu-item>
+          <el-menu-item index="">Java</el-menu-item>
+          <el-menu-item index="">Python</el-menu-item>
+        </el-submenu>
         <el-menu-item index="/competitions">比赛</el-menu-item>
         <el-menu-item index="/articles">文章</el-menu-item>
         <el-menu-item index="/discuss">讨论</el-menu-item>
         <el-submenu index="/" class="user-menu">
           <template slot="title">用户XXX</template>
-          <el-menu-item index="/user/profile">个人信息</el-menu-item>
-          <el-menu-item index="/user/submission">我的提交</el-menu-item>
           <el-menu-item index="/user/mark">我的收藏</el-menu-item>
-          <el-menu-item index="/user/security">更改密码</el-menu-item>
+          <el-menu-item index="/user/submission">历史记录/我的提交</el-menu-item>
+           <div class="line"></div>
+           <el-menu-item index="">个人战绩</el-menu-item>
+          <el-menu-item index="/user/profile">个人信息/个人积分</el-menu-item>
           <div class="line"></div>
+           <el-menu-item index="/user/security">修改密码</el-menu-item>
           <el-menu-item index="/user/logout">退出登录</el-menu-item>
         </el-submenu>
       </div>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,13 +1,35 @@
 <template>
 <el-row :gutter="10">
-  <el-col :sm="24" :md="18" class="media-list">
+  <el-col :sm="20" :md="14" class="media-list">
     <div v-for="problem in problems" class="media">
-      <h4>{{problem.user.name}} 发布了新题 {{problem.title}}</h4>
-      <p>{{problem.description}}</p>
+      <h4>
+        <i class="fa fa-file-text-o" aria-hidden="true"></i>
+        {{problem.user.name}}<span> 发布了新题</span> {{problem.title}}
+      <i class="fa fa-star" aria-hidden="true"></i>
+      </h4>
+      <p class="p2">{{problem.description}}</p>
     </div>
   </el-col>
-  <el-col :sm="0" :md="6">
-   
+  <el-col :sm="0" :md="6" offset="4">
+    <i class="fa fa-trophy fa-5x" aria-hidden="true"></i>
+    <div class="left_position">
+    <div class="title"><h>SCNU Online</h></div>
+     <div class="title"><h>Judge Contest</h></div>
+     <p class="p1">Participate and win your</p><p class="p1"> prizes</p>  
+    <div class="button"><p><el-button type="primary">Let's race!</el-button></p></div>  
+    </div>
+    <i class="fa fa-comments-o fa-5x" aria-hidden="true"></i>
+    <div class="left_position">
+    <div class="title"><h>Discuss now</h></div>
+     <p class="p1">Share interview question</p>
+     <p class="p1">Get solutions</p>
+    <div class="button"><p><el-button type="primary">Let's join us!</el-button></p></div>
+    </div>
+    <div class="left_position">
+    <div class="title"><h>Articles</h></div>
+     <p class="p1">Here is the only official editorials/solution you will find</p>
+    <p class="p1"><el-button type="primary">Welcome</el-button></p>
+    </div>
   </el-col>
 </el-row>
 </template>
@@ -15,6 +37,7 @@
 <script>
 import { mapState } from 'vuex'
 import store from 'src/store'
+import 'font-awesome/css/font-awesome.css'
 export default {
   computed: {
     ...mapState({
@@ -36,5 +59,65 @@ export default {
 .media {
   overflow: hidden;
   border-bottom: 1px solid #eee;
+}
+h{
+  text-align: center;
+  font-size: 20px;
+}
+h4{
+  color:#1d8ce0;
+  font-size: 18px;
+}
+h4 span{
+  color:#666666;
+  font-size: 18px;
+}
+p{
+  line-height: 180%;
+}
+.p1{
+  text-align: center;
+}
+.p2{
+  margin-top: 30px;
+}
+.title {
+  font-size: 20px;
+  color:#8492a6;
+  line-height: 140%;
+  text-align: center;
+}
+.button{
+  border-bottom: 1px solid #eee;
+  
+}
+.left_position{
+  float: right !important;
+  margin-top: 10px;
+}
+.fa.fa-trophy{
+  color: #ffff66;
+  height: 190px;
+  margin-top: 10px;
+  float: left !important;
+}
+.fa.fa-comments-o{
+  color: #949494;
+   margin-top: 15px;
+  float: left !important;
+}
+.fa.fa-star{
+  width: 24px;
+  height: 24px;
+  float: right !important;
+  color: #f7b900;
+}
+.fa.fa-star:hover{
+  color: #cccccc;
+}
+.fa.fa-file-text-o{
+  color: #cccccc;
+  width: 25px;
+  height: 30px;
 }
 </style>
