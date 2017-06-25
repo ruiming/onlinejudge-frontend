97354f6d5367a36c2d9fe4cb015b13382ae33016
ruiming <ruiming.zhuang@gmail.com>
2017-06-25 23:04:20 +0800

    fix: 开发服务器改回线上, 注释请求头部设置

--- src/resource.js
+++ src/resource.js
@@ -6,7 +6,7 @@ Vue.use(VueResource)
 Vue.http.options.root = process.env.API_ROOT
 
 Vue.http.interceptors.push((request, next) => {
-  request.headers.set('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwiZXhwIjoxNDk4NDYxOTI5LCJpYXQiOjE0OTgzNzU1Mjl9.7jUjYo_paMRAR7AMP1a2RNouPuO_XhFliTuUo822Wkc')
+  // request.headers.set('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwiZXhwIjoxNDk4NDYxOTI5LCJpYXQiOjE0OTgzNzU1Mjl9.7jUjYo_paMRAR7AMP1a2RNouPuO_XhFliTuUo822Wkc')
   next(response => {
     if (response.status !== 200) {
       if (response.data !== null && response.data.message) {
